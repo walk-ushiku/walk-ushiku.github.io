efe235d946e49ee06040246677177fcbd41fc26f
@nato_blooming <sodium.ionize@gmail.com>
2025-06-19 13:20:48 +0900

    修正

--- src/content/config.ts
+++ src/content/config.ts
@@ -27,6 +27,18 @@ const courseCollection = defineCollection({
         }),
 });
 
+const noteCollection = defineCollection({
+        schema: z.object({
+                title: z
+                        .string()
+                        .max(100, "The title length must be less than or equal to 100 chars"),
+                description: z.string(),
+                image: z.string().optional(),
+                icon: z.string(),
+                id: z.number(),
+        }),
+});
+
 const photoPagesCollection = defineCollection({
         schema: z.object({
 		image: z.string(),
@@ -42,4 +54,5 @@ export const collections = {
 	spot_note: spotNoteCollection,
 	course: courseCollection,
 	photo_pages: photoPagesCollection,
+	note: noteCollection,
 };
